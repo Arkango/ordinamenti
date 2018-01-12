94e7ea354513af6dddb05b53e538b56ac73b9c18
Arkango <arcangelo@debian>
2018-01-12 15:16:10 +0000

    inizio bubble sort

--- ordinamento_bubble_sort.c
+++ ordinamento_bubble_sort.c
@@ -3,3 +3,20 @@ Ordinamento di un vettore attraverso il bubble sort,
 algoritmo dimostratosi migliore ripsetto all'ordinamento normale perchè il numero di confronti eseguiti
 è minore
 */
+
+#include <stdio.h>
+
+#define NELEM 10
+static int N;
+
+typedef enum{
+    falso,vero
+}bool;
+
+//procedura che riordina il vettore
+void bubble(int VETT[]);
+
+int main(){
+    
+    return 0;
+}
