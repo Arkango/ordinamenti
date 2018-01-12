1267efc3b9ae7de34a61aa23c410d213f2873582
Arkango <arcangelo@debian>
2018-01-12 12:20:51 +0000

    primo ordinamento completo

---
+++ ordinamento_bubble_sort.c
@@ -0,0 +1,5 @@
+/*
+Ordinamento di un vettore attraverso il bubble sort,
+algoritmo dimostratosi migliore ripsetto all'ordinamento normale perchè il numero di confronti eseguiti
+è minore
+*/
--- ordinamento_vettore_semplice.c
+++ ordinamento_vettore_semplice.c
@@ -6,23 +6,52 @@ Successivamente li riordina dal più piccolo al più grande senza uso di altre s
 #include<stdlib.h>
 #include<stdio.h>
 
+
+static int N;
+
+//procedura ordinamento
+void riordina(int INPUT[]);
+
 int main(){
     const int MAXINPUT = 30;
     int INPUT[MAXINPUT];
-    int i,N;
+    int i,MIN;
 
     do{
         printf("Inserisci il numero di valori che vuoi inserire (1-30)\n");
         scanf("%d",&N);
         if(N <= 0 || N > MAXINPUT)
-            printf("valore inserito non valido");
+            printf("valore inserito non valido\n");
     }while(N <= 0 || N > MAXINPUT);
 
     for(i=0;i<N;i++){
-        printf("Inserisci valore %d",i+1);
+        printf("Inserisci valore n° %d \n",i+1);
         scanf("%d",&INPUT[i]);
     }
 
-    //create a sort
+    //riordina il vettore
+    riordina(INPUT);
 
+    printf("Vettore riordinato\n");
+    
+    for(i=0;i<N;i++){
+        printf("%d \n",INPUT[i]);
+    }
+    return 0;
+}
+
+void riordina(int INPUT[]){
+    int j,i,min,ind;
+    for(i=0;i<=N;i++){
+        min=INPUT[i];
+        ind = i;
+        for(j=i+1;j<N;j++){
+            if(INPUT[j]<min){
+                min = INPUT[j];
+                ind = j;
+            }
+        }
+        INPUT[ind] = INPUT[i];
+        INPUT[i] = min;  
+    }
 }
