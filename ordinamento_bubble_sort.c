3ca4965ac922a77190adc6d8df4eb475ced8252c
Arkango <arcangelo@debian>
2018-01-12 15:36:45 +0000

    fine buuble

--- ordinamento_bubble_sort.c
+++ ordinamento_bubble_sort.c
@@ -6,13 +6,11 @@ algoritmo dimostratosi migliore ripsetto all'ordinamento normale perchè il nume
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define NELEM 10
 static int N;
 
-typedef enum bool{
-    falso,vero
-}boolean;
 
 //procedura che riordina il vettore
 void bubble(int VETT[]);
@@ -42,6 +40,19 @@ int main(){
 //procedura che riordina il vettore attraverso il buuble sort
 void bubble(int VETT[]){
     int i,provv;
-    boolean inordine = 0;
+    bool inordine = 0;
+
+    while(!inordine){
+        inordine = 1;
+        for(i=0;i<NELEM -1;i++){
+            if(VETT[i]>VETT[i+1]){
+                provv = VETT[i];
+                VETT[i]= VETT[i+1];
+                VETT[i+1] = provv;
+                inordine = 0;
+            }
+        }
+    }
+
 
 };
