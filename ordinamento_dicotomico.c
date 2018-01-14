e796345f154a0bd75c77e26790da7084374b35f5
Arangelo <asaracino@arymait>
2018-01-14 13:43:51 +0100

    ricerca dicotomica

---
+++ ordinamento_dicotomico.c
@@ -0,0 +1,12 @@
+/* Questa ricerca permette di ridurre di molto il tmepo di esecuzione
+del programma nel caso si voglio fare una ricerca su una mole di dati molto grande
+, però è necessario prima riordinare il vettore per poi poter applicare questo tipo di ricerca
+*/
+
+// Author : Arcangelo Saracino
+// Date : 14.01.2018
+
+#include<stdlib.h>
+#include<stdio.h>
+
+#define NELEM 10
