ad0b3633d5a0d56c91b946bf493adaa9d1c6784b
Arangelo <asaracino@arymait>
2018-01-14 13:33:07 +0100

    char bubble sort finito

--- ordinamento_char_bubble_sort.c
+++ ordinamento_char_bubble_sort.c
@@ -7,48 +7,52 @@ algoritmo dimostratosi migliore ripsetto all'ordinamento normale perchè il nume
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define NELEM 10
+#define L_MAX 50
 static int N;
 
 
 //procedura che riordina il vettore
-void bubble(int VETT[]);
+void bubble(char VETT[][L_MAX]);
 
 int main(){
-    int VETT[NELEM],i;
-    printf("Introduci i valori \n");
+    char parole [NELEM][L_MAX];
+    int i;
+    printf("Introduci  le parole \n");
 
     for(i=0;i<NELEM;i++){
         //riempie il vettore con gli elementi
-        printf("Elemento di indice %d: \n",i+1);
-        scanf("%d",&VETT[i]);
+        printf("Parola di indice %d: \n",i+1);
+        fgets(parole[i],50,stdin);
     }
 
-    bubble(VETT);
+    bubble(parole);
 
     printf("\nVettore ordinato \n");
 
     for(i=0;i<NELEM;i++){
         //visualizza il vettore ordinato
-        printf("Elemento di indice %d: %d \n ",i+1,VETT[i]);
+        printf("Parola di indice %d: %s \n ",i+1,parole[i]);
     }
     
     return 0;
 }
 
 //procedura che riordina il vettore attraverso il buuble sort
-void bubble(int VETT[]){
-    int i,provv;
+void bubble(char VETT[][L_MAX]){
+    int i;
+    char provv[L_MAX];
     bool inordine = 0;
 
     while(!inordine){
         inordine = 1;
         for(i=0;i<NELEM -1;i++){
-            if(VETT[i]>VETT[i+1]){
-                provv = VETT[i];
-                VETT[i]= VETT[i+1];
-                VETT[i+1] = provv;
+            if(strcmp(VETT[i],VETT[i+1]) > 0){
+                strcpy(provv,VETT[i]);
+                strcpy(VETT[i],VETT[i+1]);
+                strcpy(VETT[i+1],provv);
                 inordine = 0;
             }
         }
